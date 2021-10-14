a00132b6ea56e4c3747ca89fcea14df2b145be82
nicolas-rodriguez-2 <a2018121014@estudiantes.upsa.edu.bo>
2021-10-13 23:33:06 -0400

    Finalizada API Random

---
+++ Magic_NicolasRodriguez/Controllers/RandomController.cs
@@ -0,0 +1,43 @@
+﻿using Magic_NicolasRodriguez.Data;
+using Magic_NicolasRodriguez.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Magic_NicolasRodriguez.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RandomController : ControllerBase
+    {
+
+        private readonly AppDbContext _context;
+        public RandomController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Suerte>> GetCancion()
+        {
+
+            var list = await _context.Suerte.ToListAsync();
+
+            var max = list.Count;
+            int index = new Random().Next(0, max);
+
+            var suerte = list[index];
+
+            if (suerte == null)
+            {
+                return NoContent();
+            }
+
+            return suerte;
+        }
+    }
+}
---
+++ Magic_NicolasRodriguez/Migrations/20211014031027_AddModel.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Magic_NicolasRodriguez.Migrations
+{
+    public partial class AddModel : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Suerte",
+                columns: table => new
+                {
+                    FuturoId = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    Vision = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Imagen = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Suerte", x => x.FuturoId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Suerte");
+        }
+    }
+}
